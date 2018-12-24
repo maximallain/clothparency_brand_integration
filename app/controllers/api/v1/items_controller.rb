55142f1424b7479f15a36cf4a52d072e23fcdd74
M3g4lodon <mathieu.seris@student.ecp.fr>
2018-12-24 07:37:58 +0100

    API de base (GET items, POST items, GET /items/:id)

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,29 @@ class Api::V1::ItemsController < Api::V1::BaseController
         # @items = policy_scope(Item)
         @items = Item.all
     end
+
+    def show
+        @item = Item.find(params[:id])
+    end
+
+    # private
+
+    def set_item
+        @item = Item.find(params[:id])
+        # authorize @restaurant # For Pundit
+    end
+
+    def item_params
+        params.permit(:name_ref)
+    end
+
+    def create
+        @item=Item.new(item_params)
+        if @item.save
+            render :show, status: :created
+        else
+            render_error
+        end
+    end
+
 end
---
+++ app/views/api/v1/items/show.json.jbuilder
@@ -0,0 +1,2 @@
+# app/views/api/v1/items/show.json.jbuilder
+json.extract! @item, :id, :name_ref
